cd875e24fc0dcc63ef37f0030bb385e68f2e9c42
Michael Bierbaumer <mb@michaelb.at>
2023-11-22 10:35:51 +0100

    made it a full day event, added reminder

--- app.py
+++ app.py
@@ -38,11 +38,21 @@ def generate_ical():
         date_str = date_str.split(' ')[0]
 
         date = datetime.strptime(date_str, '%d.%m.%Y')
+
+        # Create an event with all-day setting
         event = icalendar.Event()
         event.add('summary', event_str)
         event.add('location', 'Steyr, Austria')
         event.add('dtstart', date)
-        event.add('dtend', date + timedelta(hours=2))
+        event.add('dtend', date + timedelta(days=1))
+        event.add('x-prop-allday', '1')
+
+        # Create a reminder for the event on the day before at 7pm
+        reminder = icalendar.Alarm()
+        reminder.add('action', 'DISPLAY')
+        reminder.add('trigger', date - timedelta(days=1, hours=19))
+        reminder.add('description', 'Reminder: ' + event_str)
+        event.add_component(reminder)
 
         events.append(event)
 
